bd7b91c91839a65e028dc142fde39e5f10a926b4
Lucas Farias <lucasfnf@gmail.com>
2023-12-04 19:18:45 +0900

    Improve readability of Day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -46,11 +46,13 @@ fn parts_finder(
     should_process: &dyn Fn(u8) -> bool,
     symbol_processor: &dyn Fn(&[u32], &mut Vec<u32>),
 ) -> Result<u32, String> {
-    let line_to_gears = |window: &[u8]| -> Option<u32> {
-        if window.len() > 1 && (window[1] < b'0' || window[1] > b'9') {
+    // Searchs for a part number within slice
+    // assumes a slice of length 1 or 3
+    let search_part_number_in_slice = |slice: &[u8]| -> Option<u32> {
+        if slice.len() == 3 && (slice[1] < b'0' || slice[1] > b'9') {
             None
         } else {
-            Some(window.iter().fold(0u32, |accum, d| {
+            Some(slice.iter().fold(0u32, |accum, d| {
                 if d.is_ascii_digit() {
                     accum * 10 + d.clamp(&b'0', &b'9').saturating_sub(b'0') as u32
                 } else {
@@ -59,79 +61,123 @@ fn parts_finder(
             }))
         }
     };
-    let adjacent = |gears: &mut Vec<u32>, slice: &[u8]| -> Result<(), String> {
-        if let Some(max) = &slice[1..=5].windows(3).filter_map(line_to_gears).max() {
-            gears.push(*max);
-        }
-        Ok(())
-    };
-    let left_diagonal = |gears: &mut Vec<u32>, slice: &[u8]| -> Result<(), String> {
-        if slice[2].is_ascii_digit() {
-            let sub_slice = if slice[1].is_ascii_digit() {
-                &slice[0..=2]
-            } else {
-                &slice[2..=2]
-            };
-            if let Some(left) = line_to_gears(sub_slice) {
-                gears.push(left);
-            } else {
-                Err("Failed to get gear number where there should be one.")?
+    // Works for any of the combinations were there is a digit touching a symbol from above or below
+    // i.e. 123.. .123. ..123 ..*.. ..*.. ..*..
+    //      ..*.. ..*.. ..*.. 123.. .123. ..123
+    let search_part_number_adjacent_to_symbol =
+        |gears: &mut Vec<u32>, slice: &[u8]| -> Result<(), String> {
+            if let Some(max) = &slice[1..=5]
+                .windows(3)
+                .filter_map(search_part_number_in_slice)
+                .max()
+            {
+                gears.push(*max);
             }
+            Ok(())
         };
-
-        Ok(())
-    };
-    let right_diagonal = |gears: &mut Vec<u32>, slice: &[u8]| -> Result<(), String> {
-        if slice[4].is_ascii_digit() {
-            let sub_slice = if slice[5].is_ascii_digit() {
-                &slice[4..=6]
-            } else {
-                &slice[4..=4]
+    // Works for any position where a digit is not directly above or below a symbol and to the left
+    // i.e. 123. 123* ...*
+    //      ...* .... 123.
+    let search_part_number_left_diagonal_to_symbol =
+        |gears: &mut Vec<u32>, slice: &[u8]| -> Result<(), String> {
+            if slice[2].is_ascii_digit() {
+                let sub_slice = if slice[1].is_ascii_digit() {
+                    &slice[0..=2]
+                } else {
+                    &slice[2..=2]
+                };
+                if let Some(left) = search_part_number_in_slice(sub_slice) {
+                    gears.push(left);
+                } else {
+                    Err("Failed to get gear number where there should be one.")?
+                }
             };
-            if let Some(right) = line_to_gears(sub_slice) {
-                gears.push(right);
-            } else {
-                Err("Failed to get gear number where there should be one.")?
-            }
-        };
 
-        Ok(())
-    };
+            Ok(())
+        };
+    // Works for any position where a digit is not directly above or below a symbol and to the right
+    // i.e. .123 *123 *...
+    //      *... .... .123
+    let search_part_number_right_diagonal_to_symbol =
+        |gears: &mut Vec<u32>, slice: &[u8]| -> Result<(), String> {
+            if slice[4].is_ascii_digit() {
+                let sub_slice = if slice[5].is_ascii_digit() {
+                    &slice[4..=6]
+                } else {
+                    &slice[4..=4]
+                };
+                if let Some(right) = search_part_number_in_slice(sub_slice) {
+                    gears.push(right);
+                } else {
+                    Err("Failed to get gear number where there should be one.")?
+                }
+            };
+
+            Ok(())
+        };
 
     match input.chars().position(|c| c == '\n') {
         Some(chars_per_line) => {
+            // "Empty" line with the same length as the lines from the input
             let all_dots = String::from_iter(".".chars().cycle().take(chars_per_line));
-            let lines = [all_dots.as_str()]
+            // List of lines with "empty" line padding at the beginning and end
+            let lines_with_padding = [all_dots.as_str()]
                 .into_iter()
                 .chain(input.split_whitespace())
                 .chain([all_dots.as_str()])
                 .map(|s| s.as_bytes())
                 .collect::<Vec<_>>();
-            lines
+
+            lines_with_padding
                 .windows(3)
                 .try_fold(vec![], |mut parts: Vec<u32>, cur| {
                     if let [top, mid, bottom] = cur {
-                        // Possible because input does not have '*' close to the start or end of the lines
-                        for ((a, b), c) in top.windows(7).zip(mid.windows(7)).zip(bottom.windows(7))
+                        // Possible because input does not have symbols close to the start or end of the lines
+                        for ((window_on_top_line, window_on_mid_line), window_on_bottom_line) in
+                            top.windows(7).zip(mid.windows(7)).zip(bottom.windows(7))
                         {
-                            if should_process(b[3]) {
+                            if should_process(window_on_mid_line[3]) {
                                 let mut adjacent_parts = vec![];
                                 // Top line tests
-                                if a[3].is_ascii_digit() {
-                                    adjacent(&mut adjacent_parts, a)?;
+                                if window_on_top_line[3].is_ascii_digit() {
+                                    search_part_number_adjacent_to_symbol(
+                                        &mut adjacent_parts,
+                                        window_on_top_line,
+                                    )?;
                                 } else {
-                                    left_diagonal(&mut adjacent_parts, a)?;
-                                    right_diagonal(&mut adjacent_parts, a)?;
+                                    search_part_number_left_diagonal_to_symbol(
+                                        &mut adjacent_parts,
+                                        window_on_top_line,
+                                    )?;
+                                    search_part_number_right_diagonal_to_symbol(
+                                        &mut adjacent_parts,
+                                        window_on_top_line,
+                                    )?;
                                 }
                                 // Mid line tests
-                                left_diagonal(&mut adjacent_parts, b)?;
-                                right_diagonal(&mut adjacent_parts, b)?;
+                                search_part_number_left_diagonal_to_symbol(
+                                    &mut adjacent_parts,
+                                    window_on_mid_line,
+                                )?;
+                                search_part_number_right_diagonal_to_symbol(
+                                    &mut adjacent_parts,
+                                    window_on_mid_line,
+                                )?;
                                 // Bottom line tests
-                                if c[3].is_ascii_digit() {
-                                    adjacent(&mut adjacent_parts, c)?;
+                                if window_on_bottom_line[3].is_ascii_digit() {
+                                    search_part_number_adjacent_to_symbol(
+                                        &mut adjacent_parts,
+                                        window_on_bottom_line,
+                                    )?;
                                 } else {
-                                    left_diagonal(&mut adjacent_parts, c)?;
-                                    right_diagonal(&mut adjacent_parts, c)?;
+                                    search_part_number_left_diagonal_to_symbol(
+                                        &mut adjacent_parts,
+                                        window_on_bottom_line,
+                                    )?;
+                                    search_part_number_right_diagonal_to_symbol(
+                                        &mut adjacent_parts,
+                                        window_on_bottom_line,
+                                    )?;
                                 }
 
                                 symbol_processor(&adjacent_parts, &mut parts);
@@ -143,7 +189,7 @@ fn parts_finder(
                         Err("Windows did not have 3 parts.".to_owned())
                     }
                 })
-                .map(|ratios| ratios.into_iter().sum())
+                .map(|part_numbers_or_gear_ratios| part_numbers_or_gear_ratios.into_iter().sum())
         }
         None => Err("Input had no lines.".to_owned()),
     }
